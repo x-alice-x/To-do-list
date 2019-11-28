9e81a2ea9c28cf2707d039e0b828aa414c922456
Alice <alisa.4831@gmail.com>
2019-11-28 19:32:19 +0700

    seems ok

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDbKU6zJS4YDFVgNrh9W-jCQTqU1cko8Zg",
+    authDomain: "todo-list-a79e0.firebaseapp.com",
+    databaseURL: "https://todo-list-a79e0.firebaseio.com",
+    projectId: "todo-list-a79e0",
+    storageBucket: "todo-list-a79e0.appspot.com",
+    messagingSenderId: "1072021065123",
+    appId: "1:1072021065123:web:f67493592d8dea4299de16"
+  };
